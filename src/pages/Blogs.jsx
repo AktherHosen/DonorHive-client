41b619a822d089ff0c74d1bc9755ebeba0da9ae5
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-12 12:50:56 +0600

    published blog page added

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import toast from "react-hot-toast";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await axios.get(
+          `${import.meta.env.VITE_API_URL}/all-blogs?status=published`
+        );
+        setBlogs(result.data);
+      } catch (err) {
+        toast.error(err?.message);
+      }
+    };
+    getData();
+  }, []);
   return (
     <div>
       <Helmet>
         <title>Blogs</title>
       </Helmet>
-      <h1>Blogs are coming soon.</h1>
+      <h1>Blogs are coming soon.{blogs.length}</h1>
     </div>
   );
 };
