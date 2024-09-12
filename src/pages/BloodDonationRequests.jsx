aa23c108281c309a7f9ecb0b687a383b7d2267b4
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-13 00:19:26 +0600

    all pending donation request page added with functionality of going to details

--- src/pages/BloodDonationRequests.jsx
+++ src/pages/BloodDonationRequests.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import toast from "react-hot-toast";
-
+import { Link } from "react-router-dom";
+import { TbListDetails } from "react-icons/tb";
 const BloodDonationRequests = () => {
   const [donationRequests, setDonationRequests] = useState([]);
   useEffect(() => {
@@ -27,6 +28,39 @@ const BloodDonationRequests = () => {
       </Helmet>
 
       <h1>Pending donation request are shwon here {donationRequests.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table border">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Recipient Name</th>
+              <th>Location</th>
+              <th>Donation Date</th>
+              <th>Donation Time</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {donationRequests.map((dn) => (
+              <tr key={dn._id} className="hover:bg-gray-100">
+                <td>{dn.recipientName}</td>
+                <td>{dn.fullAddress}</td>
+                <td>{dn.donationDate}</td>
+                <td>{dn.donationTime}</td>
+                <td>
+                  <Link to={`/dashboard/donation-request-details/${dn._id}`}>
+                    <TbListDetails
+                      title="Details"
+                      size={20}
+                      className="hover:text-primary hover:scale-110 hover:transition-all hover:duration-300"
+                    />
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
