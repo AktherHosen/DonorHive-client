79fa901ffbb5bbc76381a6ae8922d8a325d841d5
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-14 23:32:18 +0600

    added role based feature in content management page

--- src/pages/Dashboard/Contents.jsx
+++ src/pages/Dashboard/Contents.jsx
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 import { MdDelete } from "react-icons/md";
 import toast from "react-hot-toast";
 import { IoAddCircleSharp } from "react-icons/io5";
+import useAdmin from "../../hooks/useAdmin";
+
 const Contents = () => {
   const [blogs, setBlogs] = useState([]);
   const [filter, setFilter] = useState("");
+  const { isAdmin, isVolunteer } = useAdmin();
 
   const getData = async () => {
     try {
@@ -18,43 +21,48 @@ const Contents = () => {
       console.log(err?.message);
     }
   };
+
   const handleStatus = async (id, status) => {
-    // console.log(id, status);
-    const newStatus = status === "draft" ? "published" : "draft";
+    const newStatus = status === "published" ? "draft" : "published";
+    if (status === "draft" && !isAdmin) return;
+
     try {
-      const result = await axios.patch(
-        `${import.meta.env.VITE_API_URL}/blog/${id}`,
-        { status: newStatus }
-      );
+      await axios.patch(`${import.meta.env.VITE_API_URL}/blog/${id}`, {
+        status: newStatus,
+      });
       getData();
     } catch (err) {
       console.log(err?.message);
     }
   };
+
   const handleDelete = async (id) => {
+    if (!isAdmin) return;
+
     try {
-      const result = await axios.delete(
-        `${import.meta.env.VITE_API_URL}/blog/${id}`
-      );
+      await axios.delete(`${import.meta.env.VITE_API_URL}/blog/${id}`);
       toast.success("Blog Deleted Successfully.");
       getData();
     } catch (err) {
       console.log(err?.message);
     }
   };
+
   useEffect(() => {
     getData();
   }, [filter]);
+
   return (
-    <div className="pr-8 w-96 md:w-full lg:w-ful">
+    <div className="pr-8 w-96 md:w-full lg:w-full">
       <div className="flex justify-end">
         <Link
           to="add-blog"
-          className="border rounded-full text-lg shadow-md p-0.5 hover:text-primary  font-semibold"
+          className="border rounded-full text-lg shadow-md p-0.5 hover:text-primary font-semibold"
         >
           <IoAddCircleSharp size={30} />
         </Link>
       </div>
+
       <div className="flex justify-center my-2">
         <div>
           <label htmlFor="filterStatus" className="label block text-xs">
@@ -73,81 +81,77 @@ const Contents = () => {
           </select>
         </div>
       </div>
+
       <div>
         {blogs.length > 0 ? (
-          <>
-            <div className="overflow-x-auto">
-              <table className="table table-xs">
-                <thead>
-                  <tr>
-                    <th>Thumbnail</th>
-                    <th>Blog Title</th>
-                    <th>Content</th>
-                    <th>Status</th>
-                    <th>Manage Status</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {blogs.map((blog) => (
-                    <tr key={blog._id}>
-                      <th>
-                        <img
-                          src={blog.thumb}
-                          className="h-12 w-20 rounded-md"
-                          alt=""
-                        />
-                      </th>
-                      <td>{blog.title}</td>
-                      <td
-                        dangerouslySetInnerHTML={{
-                          __html:
-                            blog.descriptionContent.length > 60
-                              ? `${blog.descriptionContent.substring(0, 70)}...`
-                              : blog.descriptionContent,
-                        }}
+          <div className="overflow-x-auto">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th>Thumbnail</th>
+                  <th>Blog Title</th>
+                  <th>Content</th>
+                  <th>Status</th>
+                  <th>Manage Status</th>
+                  {isAdmin && <th>Actions</th>}{" "}
+                </tr>
+              </thead>
+              <tbody>
+                {blogs.map((blog) => (
+                  <tr key={blog._id}>
+                    <td>
+                      <img
+                        src={blog.thumb}
+                        className="h-12 w-20 rounded-md"
+                        alt=""
                       />
-                      <td>{blog.status}</td>
-                      <td>
-                        <div className="flex items-center space-x-2">
-                          {blog.status === "draft" ? (
-                            <button
-                              onClick={() =>
-                                handleStatus(blog._id, blog.status)
-                              }
-                              className="flex items-center gap-2 px-3 py-1 rounded-full text-xs bg-slate-50 text-primary font-semibold"
-                            >
-                              <div className="w-1 h-1 bg-primary rounded-full"></div>{" "}
-                              publish
-                            </button>
-                          ) : (
-                            <button
-                              onClick={() =>
-                                handleStatus(blog._id, blog.status)
-                              }
-                              className="flex items-center gap-2 px-3 py-1 rounded-full text-xs bg-slate-50 text-primary font-semibold"
-                            >
-                              <div className="w-1 h-1 bg-primary rounded-full"></div>{" "}
-                              unpublish
-                            </button>
-                          )}
-                        </div>
-                      </td>
-
+                    </td>
+                    <td>{blog.title}</td>
+                    <td
+                      dangerouslySetInnerHTML={{
+                        __html:
+                          blog.descriptionContent.length > 60
+                            ? `${blog.descriptionContent.substring(0, 70)}...`
+                            : blog.descriptionContent,
+                      }}
+                    />
+                    <td>{blog.status}</td>
+                    <td>
+                      {blog.status === "draft" ? (
+                        isAdmin && (
+                          <button
+                            onClick={() => handleStatus(blog._id, blog.status)}
+                            className="flex items-center gap-2 px-3 py-1 rounded-full text-xs bg-slate-50 text-primary font-semibold"
+                          >
+                            <div className="w-1 h-1 bg-primary rounded-full"></div>{" "}
+                            publish
+                          </button>
+                        )
+                      ) : (
+                        <button
+                          onClick={() => handleStatus(blog._id, blog.status)}
+                          className="flex items-center gap-2 px-3 py-1 rounded-full text-xs bg-slate-50 text-primary font-semibold"
+                        >
+                          <div className="w-1 h-1 bg-primary rounded-full"></div>{" "}
+                          unpublish
+                        </button>
+                      )}
+                    </td>
+                    {isAdmin && (
                       <td>
                         <button
                           onClick={() => handleDelete(blog._id)}
-                          className="px-2 py-1 rounded-md  bg-slate-100 text-primary text-xl"
+                          className="px-2 py-1 rounded-md bg-slate-100 text-primary text-xl"
                         >
                           <MdDelete />
                         </button>
                       </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>{" "}
-          </>
+                    )}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         ) : (
           <p className="text-gray-500 font-semibold text-start mt-4 text-sm">
             No Blogs Found!
