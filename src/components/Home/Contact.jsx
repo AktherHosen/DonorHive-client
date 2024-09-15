ca979e0f17349a6fa7c65460f2c486d26b41efaf
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-15 22:20:13 +0600

    added footer page

---
+++ src/components/Home/Contact.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { MdMarkEmailUnread, MdPermPhoneMsg } from "react-icons/md";
+import { BsSendFill } from "react-icons/bs";
+const Contact = () => {
+  return (
+    <div className="my-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-4">
+        <div>
+          <h1 className="text-3xl font-semibold mb-4">Let's Donate Together</h1>
+          <div className="flex flex-col justify-center space-y-4">
+            <p>
+              If you have any questions, need assistance, or want to learn more
+              about our services, feel free to reach out! We're here to help.
+              You can contact us via phone, email, or through our online form,
+              and a member of our team will get back to you as soon as possible.
+              We look forward to hearing from you!
+            </p>
+            <div>
+              <h1 className="flex items-center gap-2">
+                <MdMarkEmailUnread size={20} />
+                <span className="font-semibold text-lg"> donor@hive.com</span>
+              </h1>
+              <h1 className="flex items-center gap-2">
+                <MdPermPhoneMsg size={20} />
+                <span className="font-semibold text-lg">
+                  {" "}
+                  (+880)-1610945101
+                </span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold uppercase">Contact Us</h1>
+          <form>
+            <div className="flex flex-col lg:flex-row gap-2">
+              <div className="lg:w-1/2">
+                <label htmlFor="name" className="block text-xm label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="border px-4 py-3 w-full outline-none"
+                />
+              </div>
+              <div className="lg:w-1/2">
+                <label htmlFor="email" className="block text-xm label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="border px-4 py-3 w-full outline-none"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="name" className="block text-xm label">
+                Message
+              </label>
+              <textarea
+                name=""
+                placeholder="Enter your message"
+                id=""
+                className="border px-4 py-3 w-full resize-none h-32 outline-none"
+              ></textarea>
+            </div>
+            <button className="bg-primary flex items-center gap-2 text-white rounded-md font-semibold px-4 py-2 mt-2">
+              Send
+              <BsSendFill size={16} />
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,94 @@
 import React from "react";
-
+import { FaLocationArrow, FaPhoneAlt } from "react-icons/fa";
+import { IoMailUnreadSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
 const Footer = () => {
-  return <div>this is footer</div>;
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    toast.success("Thank you for subscribing our Newsletter.");
+    e.target.reset();
+  };
+  return (
+    <div className="bg-gray-900 text-white text-opacity-80 mt-10 ">
+      <footer className="px-4 lg:px-36 text-white py-10  ">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+          <div className="col-span-2 mb-8 lg:mb-0 space-y-3">
+            <h1 className="text-3xl font-semibold">
+              Donor Hive{" "}
+              <span className="text-primary font-bold text-4xl">.</span>
+            </h1>
+            <p className="text-gray-400 mb-4 text-sm">
+              Thank you for visiting our blood donation website. Together, we
+              can save lives and make a positive impact on those in need. Stay
+              connected with us through our social media channels or contact us
+              for more information. Your support and generosity help ensure that
+              every drop counts. Let’s build a healthier and stronger
+              community—one donation at a time
+            </p>
+            <ul className="text-gray-400">
+              <li className="flex items-center">
+                <div className="flex items-center gap-2">
+                  <IoMailUnreadSharp /> donor@hive.com
+                </div>
+              </li>
+              <li className="flex items-center mb-2">
+                <div className="flex items-center gap-2">
+                  <FaPhoneAlt /> (+880) - 1610945101
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div className=" mb-8 lg:mb-0">
+            <h3 className="font-semibold mb-4">Quick Links</h3>
+            <ul className="text-gray-400 space-y-2">
+              <li>
+                <Link to="/donors" className="hover:text-white">
+                  Search Donor
+                </Link>
+              </li>
+              <li>
+                <Link to="/funding" className="hover:text-white">
+                  Donate Fund
+                </Link>
+              </li>
+              <li>
+                <Link to="/blogs" className="hover:text-white">
+                  Read Blogs
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" className="hover:text-white">
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link to="/registration" className="hover:text-white">
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h3 className="font-semibold mb-4">Subscribe</h3>
+            <form onSubmit={handleSubscribe}>
+              <input
+                type="text"
+                className="px-2 py-3 rounded-tl-xl w-full text-black rounded-br-xl bg-gray-100 outline-none"
+                placeholder="Enter your email"
+              />
+
+              <button className="bg-primary mt-2 rounded-tl-xl rounded-br-xl text-white px-6 py-2 rounded hover:scale-105 ">
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
